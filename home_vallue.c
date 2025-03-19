87f2558f8c034f2b5e8a8595f1b97be95af06e7b
zeynepazrayildirim <yzayza20002000@gmail.com>
2025-03-19 09:24:00 +0300

    Update home_vallue.c

--- home_vallue.c
+++ home_vallue.c
@@ -4,14 +4,14 @@
 int
 main(void)
 {
-    double popularity, //stands for the popularity of the neighborhood
+    int popularity, //stands for the popularity of the neighborhood
            size,  // stands for the size of the house
            homeValue; // stands for the value of the house
     
     printf("Please enter the popularity of the neighborhood: \n");
-    scanf("%lf", &popularity);
+    scanf("%d", &popularity);
     printf("Please enter the size of the house: \n");
-    scanf("%lf", &size);
+    scanf("%d", &size);
     homeValue = (pow(popularity, 3)+ pow(size, 2))* 10000;
     printf("The home value of the house is: %f", homeValue);
 }
